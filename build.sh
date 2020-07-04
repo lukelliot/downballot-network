92cc8ecfb73db6003a9fc34becc14a8ed8812fe0
lukelliot <lucaselliotchin@gmail.com>
2020-07-04 10:57:48 -0400

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+CYAN='\033[0;36m'
+GREEN='\033[0;32m'
+echo -e "${CYAN}"
+
 # Build webpack output
 eval "./node_modules/.bin/react-scripts build"
 
 # If server/build dir exists; then remove server/build dir
-[ -d "./server/build" ] && eval "rm -r ./server/build"
+
+if [ -d "./server/build" ]; then
+  echo -e "${CYAN}Destroying ./server/build directory..."
+  eval "rm -r ./server/build"
+  echo -e "${GREEN}Successfully removed ./server/build directory!\n"
+fi
 
 # Move new /build dir to /server
+echo -e "${CYAN}Moving Webpack bundle to ./server/build..."
 eval "mv build server/build"
+echo -e "${GREEN}Successfully moved Webpack bundle!"
