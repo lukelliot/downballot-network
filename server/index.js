76274c11995c9dba473e3b0b401c25d8c6a9bcf4
lukelliot <lucaselliotchin@gmail.com>
2020-07-04 13:31:41 -0400

    move morgan log format to server/util

--- server/index.js
+++ server/index.js
@@ -1,21 +1,24 @@
-const express = require('express');
-// const bodyParser = require('body-parser')
+const express = require('express')
 const morgan = require('morgan')
-const path = require('path');
-const app = express();
-const PORT = process.env.PORT || 8080
+const path = require('path')
+const logFormat = require('./util/logFormat')
 
-app.use(morgan(':method :url :status [:response-time ms] [:date[clf]] '))
-app.use(express.static(path.join(__dirname, 'build')));
+const app = express()
+const PORT = process.env.PORT || 8080
+const ENV = process.env.NODE_ENV || 'development'
+
+if (ENV === 'development') app.use(morgan(logFormat))
+
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/ping', (req, res) =>
   res.json({ pong: true })
 )
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, () => {
-  console.log('\x1b[35m%s\x1b[0m', `Starting server on port ${PORT}`)
+  console.log('\x1b[36m%s\x1b[0m', `[${ENV}] Starting server on port ${PORT}.`)
 })
