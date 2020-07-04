b71446444dabd5f7a63f1b7025569bae0d87b9bd
lukelliot <lucaselliotchin@gmail.com>
2020-07-04 19:59:59 -0400

    cleanup

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
-const logFormat = require('./util/logFormat')
-const { ENVIRONMENT } = require('./constants')
+const logFormat = require('./server/util/logFormat')
+const { ENVIRONMENT } = require('./server/constants')
 
-require('dotenv').config({ path: path.join(__dirname, '/.env') })
 
 const port = process.env.PORT || 8080
 const env = process.env.NODE_ENV || ENVIRONMENT.DEVELOPMENT
+
+require('dotenv').config({ path: path.join(__dirname, 'server', `.env.${env}`) })
 
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
 if (env === ENVIRONMENT.DEVELOPMENT) app.use(morgan(logFormat))
 
-app.get('/ping', (req, res) =>
+app.get('/ping', (req, res) =>{
   res.json({ pong: true })
-)
+})
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
