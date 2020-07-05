9abb6b1900ed7d4e4dc39adbd75824a41461e826
lukelliot <lucaselliotchin@gmail.com>
2020-07-04 20:10:49 -0400

    server/index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const path = require('path')
-const logFormat = require('./server/util/logFormat')
-const { ENVIRONMENT } = require('./server/constants')
+const logFormat = require('./util/logFormat')
+const { ENVIRONMENT } = require('./constants')
 
 const port = process.env.PORT || 8080
 const env = process.env.NODE_ENV || ENVIRONMENT.DEVELOPMENT
 
-require('dotenv').config({ path: path.join(__dirname, 'server', `.env.${env}`) })
+require('dotenv').config({ path: path.join(__dirname, `.env.${env}`) })
 
 const app = express()
 
